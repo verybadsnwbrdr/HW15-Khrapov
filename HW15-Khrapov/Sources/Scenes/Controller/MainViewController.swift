f57d1a1d3f9f79afd7f55e9784721adabf1c8966
Anton Khrapov <froe054@mail.ru>
2022-09-19 17:44:03 +0700

    Start filling

--- HW15-Khrapov/Sources/Scenes/Controller/MainViewController.swift
+++ HW15-Khrapov/Sources/Scenes/Controller/MainViewController.swift
@@ -7,23 +7,37 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+final class MainViewController: UIViewController {
+    
+    // MARK: - Model
+    
+    var model: CellModels? // Как не передовать модель во VIEW?
+    
+    // MARK: - ViewConfigure
+    
+    private func configureView(with model: CellModels) {
+        //  Надо обрашаться к view?
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - View
+    
+    private var mainView: MainView? {
+        isViewLoaded ? view as? MainView : nil
     }
-    */
 
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupController()
+        configureView(with: <#T##CellModels#>)
+    }
+    
+    // MARK: - Setup
+    
+    private func setupController() {
+        title = "Настройки"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        view = MainView()
+    }
 }
--- HW15-Khrapov/Sources/Scenes/View/MainView.swift
+++ HW15-Khrapov/Sources/Scenes/View/MainView.swift
@@ -6,15 +6,70 @@
 //
 
 import UIKit
+import SnapKit
 
-class MainView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+final class MainView: UIView {
+    
+    // MARK: - Configuration
+    
+//    func configureView
+    
+    // MARK: - Elements
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .insetGrouped)
+        
+//        tableView.register(SwitchTableViewCell.self, forCellReuseIdentifier: SwitchTableViewCell.identifier)
+//        tableView.register(TextTableViewCell.self, forCellReuseIdentifier: TextTableViewCell.identifier)
+//        tableView.register(TableViewCell.self, forCellReuseIdentifier: TableViewCell.identifier)
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        return tableView
+    }()
+    
+    // MARK: - Initializers
+    
+    init() {
+        super.init(frame: .zero)
+        commonInit()
     }
-    */
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func commonInit() {
+        setupHierarchy()
+        setupLayout()
+    }
+    
+    // MARK: - Setup
+    
+    private func setupHierarchy() {
+        addSubview(tableView)
+    }
+    
+    private func setupLayout() {
+        tableView.snp.makeConstraints { make in
+            make.top.bottom.left.right.equalTo(self)
+        }
+    }
+}
 
+extension MainView: UITableViewDataSource, UITableViewDelegate {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
 }
