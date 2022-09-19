9d59f1fc025e8fcd8398f27e3647eb690c7d9717
Anton Khrapov <froe054@mail.ru>
2022-09-19 16:07:55 +0700

    Add groups

---
+++ HW15-Khrapov/Sources/App/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  HW15-Khrapov
+//
+//  Created by Anton on 12.09.2022.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
+
